1730e6c20009eefc12a566c8bb5b37f36e03352b
Yury Zholobov <yuzholob@microsoft.com>
2016-04-06 18:55:23 -0700

    some interface design

--- ATAT/ATAT.Lib/Class1.cs
+++ ATAT/ATAT.Lib/Class1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
 
 namespace ATAT.Lib
 {
@@ -13,10 +14,55 @@ namespace ATAT.Lib
         private const int MaxBlocks = 16384;
 
         private Block[] _blocks;
+
+        public class Block
+        {
+            private readonly string[] _lines = new string[MaxLinesPerBlock];
+            private int _count;
+
+            public void AppendLine(string s)
+            {
+                _lines[_count] = s;
+                ++_count;
+            }
+        }
     }
 
-    public class Block
+
+    public interface IDataImporter
     {
-        private string[] _lines;
+        void Clear();
+        void AppendLine(string s);
+    }
+
+    public interface IFilterSettings
+    {
+        List<IFilter> Filters { get; }
+    }
+
+    public interface ISearcher
+    {
+        int GetCount();
+        IEnumerable<ILine> GetLinesRange(int from, int to);
+    }
+
+    /// <summary>
+    /// Represents a single "filter" definition. Filters are organaized into
+    /// more complex structures to supports complex filtering behaviors.
+    /// A single filter by itself does not fully define filtering semantics -
+    /// the whole list or tree or some other structure of filters does.
+    /// </summary>
+    public interface IFilter
+    {
+        string SearchString { get; set; }
+    }
+
+    /// <summary>
+    /// Designed for rendering. It is not "live". When you get it you get snapshot of
+    /// calculated data for rendering at the moment.
+    /// </summary>
+    public interface ILine
+    {
+
     }
 }
